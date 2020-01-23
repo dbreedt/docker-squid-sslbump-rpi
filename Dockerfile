a1a04f24768ca15d3ff59470deb15b5395de1a71
Justin Schwartzbeck <jusschwa@Denver>
2020-01-23 04:00:55 +0000

    Adds support for safe search and transparent proxy mode

--- Dockerfile
+++ Dockerfile
@@ -3,6 +3,8 @@ MAINTAINER Justin Schwartzbeck <justinmschw@gmail.com>
 
 ENV SQUID_USER=squid
 ENV SQUID_DIR /usr/local/squid
+ENV SQUID_LINK http://www.squid-cache.org/Versions/v3/3.5/squid-3.5.27.tar.gz
+ENV SQUID_VERSION 3.5.27
 
 RUN apt-get update && \
     apt-get -qq -y install openssl libssl1.0-dev build-essential wget curl net-tools dnsutils tcpdump && \
@@ -32,11 +34,16 @@ RUN echo "sslproxy_cert_error allow all" >> $SQUID_DIR/etc/squid.conf
 RUN echo "sslproxy_flags DONT_VERIFY_PEER" >> $SQUID_DIR/etc/squid.conf
 RUN sed "/^http_port 3128$/d" -i $SQUID_DIR/etc/squid.conf
 RUN sed "s/^http_access allow localnet$/http_access allow all/" -i $SQUID_DIR/etc/squid.conf
+RUN sed "/^http_port 3130 intercept" -i $SQUID_DIR/etc/squid.conf
+RUN echo "https_port 3131 intercept ssl-bump generate-host-certificates=on dynamic_cert_mem_cache_size=4MB cert=$SQUID_DIR/ssl/bluestar.crt key=$SQUID_DIR/ssl/bluestar.pem" >> $SQUID_DIR/etc/squid.conf
 RUN echo "http_port 3128 ssl-bump generate-host-certificates=on dynamic_cert_mem_cache_size=4MB cert=$SQUID_DIR/ssl/bluestar.crt key=$SQUID_DIR/ssl/bluestar.pem" >> $SQUID_DIR/etc/squid.conf
 RUN cat $SQUID_DIR/etc/squid.conf | grep added\ config -A1000 #fflush()
 
 
 EXPOSE 3128
+# For transparent proxy we are using the following ports
+EXPOSE 3130
+EXPOSE 3131
 
 ADD ./entrypoint.sh /entrypoint.sh
 RUN chmod +x /entrypoint.sh
--- entrypoint.sh
+++ entrypoint.sh
@@ -10,5 +10,32 @@ if [ ! -f $SQUID_DIR/ssl/bluestar.pem ]; then
     openssl x509 -in $SQUID_DIR/ssl/bluestar.crt -outform DER -out $SQUID_DIR/ssl/bluestar.der
 fi
 
+if [ $SAFESEARCH ]; then
+	echo "216.239.38.120 youtube.com" >> /etc/hosts
+	echo "216.239.38.120 www.youtube.com" >> /etc/hosts
+	echo "216.239.38.120 m.youtube.com" >> /etc/hosts
+	echo "216.239.38.120 youtubei.googleapis.com" >> /etc/hosts
+	echo "216.239.38.120 youtube.googleapis.com" >> /etc/hosts
+	echo "216.239.38.120 www.youtube-nocookie.com" >> /etc/hosts
+	echo "216.239.38.120 www.google.com" >> /etc/hosts
+fi
+
+cleanup() {
+    iptables -t nat -D OUTPUT -p tcp --syn --dport 80 -j REDIRECT --to-port 3130
+    iptables -t nat -D PREROUTING -p tcp --syn --dport 80 -j REDIRECT --to-port 3130
+    iptables -t nat -D OUTPUT -p tcp --syn --dport 443 -j REDIRECT --to-port 3131
+    iptables -t nat -D PREROUTING -p tcp --syn --dport 443 -j REDIRECT --to-port 3131
+    iptables -t nat -D OUTPUT -m owner --uid-owner squid -j RETURN
+}
+trap cleanup EXIT
+cleanup
+
+if [ $TRANSPARENT ]; then
+    iptables -t nat -I OUTPUT 1 -p tcp --syn --dport 80 -j REDIRECT --to-port 3130
+    iptables -t nat -I PREROUTING -p tcp --syn --dport 80 -j REDIRECT --to-port 3130
+    iptables -t nat -I OUTPUT 1 -p tcp --syn --dport 443 -j REDIRECT --to-port 3131
+    iptables -t nat -I PREROUTING -p tcp --syn --dport 443 -j REDIRECT --to-port 3131
+    iptables -t nat -I OUTPUT 1 -m owner --uid-owner squid -j RETURN
+fi
 
 exec $SQUID_DIR/sbin/squid -f $SQUID_DIR/etc/squid.conf -NYCd 10
